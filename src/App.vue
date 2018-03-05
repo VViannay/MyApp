b726e4e9551550fc5bdef01ccd07f5c99fa98fea
Vincent Viannay <vincent.viannay@Digital-Grenoble.local>
2018-03-05 11:42:39 +0100

    new version

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <p>Use the checkbox for active/desactive:</p>
-    <li v-for="mach in machines">
-      <input v-model="mach.active" type="checkbox">
+    <h3>Display Or Hide Machine Inactive</h3>
+    <toggle-button v-model="hideInactiveMachine"labels="false"/>
+    <p>Use the Toggle for active/desactive:</p>
+    <li  v-for="mach in machines" v-show="!hideInactiveMachine || mach.active">
+      <toggle-button v-model="mach.active" labels="false"/>
       <h3 :class="{inactive: !mach.active}">{{mach.machines}}</h3>
     </li>
   </div>
@@ -16,14 +18,25 @@ export default {
   data: function () {
     return {
       msg: 'List of Coffee Machines',
+      hideInactiveMachine: true,
       machines: [
-        {machines: 'Nesspresso Coffee :',
-         active: true
+        {machines: 'Nesspresso Coffee',
+         active: true,
+
         },
-        {machines: 'Moulinex Coffee :',
-         active:false
+        {machines: 'Royal  Coffee',
+         active:false,
         },
 
+        {machines: 'Tassimo Coffee',
+          active: true,
+
+        },
+
+        {machines: 'Senseo Coffee',
+          active: true,
+
+        },
       ],
 
     }
